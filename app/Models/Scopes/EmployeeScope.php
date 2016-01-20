622850e241cce7940e950dbb67332d1d8363b32e
chelsymooy <chelsymooy1108@gmail.com>
2016-01-20 15:43:28 +0700

    Fixed #10

--- app/Models/Scopes/EmployeeScope.php
+++ app/Models/Scopes/EmployeeScope.php
@@ -22,6 +22,7 @@ class EmployeeScope implements ScopeInterface
 	public function apply(Builder $builder, Model $model)
 	{
     	$builder->selectraw('CONCAT(charts.name, " cabang ", branches.name) as current_job')
+    			->selectraw('works.grade as current_grade')
     			->join('works', function ($join) 
 				 {
 	                                    $join->on ( 'persons.id', '=', 'works.person_id' )
