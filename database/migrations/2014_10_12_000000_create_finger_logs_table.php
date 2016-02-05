5d9651706b1c25aa2359bb0b434e988323cf6c4a
chelsymooy <chelsymooy1108@gmail.com>
2016-02-05 17:31:04 +0700

    Adding finger logs table

---
+++ database/migrations/2014_10_12_000000_create_finger_logs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFingerLogsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('finger_logs', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('person_id')->unsigned()->index();
+			$table->integer('created_by')->unsigned()->index();
+			$table->string('name', 255);
+			$table->datetime('on');
+			$table->datetime('last_input_time')->nullable();
+			$table->string('pc', 255);
+			$table->string('app_version', 255);
+			$table->string('ip', 255);
+			$table->timestamps();
+			$table->softDeletes();
+			
+			$table->index(['deleted_at', 'person_id', 'on']);
+
+			$table->index(['deleted_at', 'on']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('finger_logs');
+	}
+
+}
